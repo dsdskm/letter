86e43c80b601ce76f3f3b30e60751219794b0dba
kkh.kim <kkh.kim@lge.com>
2024-11-13 01:38:19 +0900

    patch

--- src/views/LoginView.tsx
+++ src/views/LoginView.tsx
@@ -1,16 +1,15 @@
 import { Box, Button, TextField, Typography, useTheme } from "@mui/material"
 import { ROUTE_CONTENTS_EDIT_VIEW, ROUTE_CONTENTS_LIST_VIEW } from "common/constants"
-import { useState } from "react"
+import { useEffect, useLayoutEffect, useState } from "react"
 import { useDispatch } from "react-redux"
 import { useNavigate } from "react-router-dom"
 import { setAccount } from "state/states"
 import Loading from "./components/Loading"
 import { LABEL, STYLE } from "common/resources"
 import { getAccount } from "api/api"
-import dotenv from "dotenv";
+import bgLogin from "images/background_login.png"
 
 const LoginView = () => {
-    console.log(`url ${process.env.SERVER_ADDRESS}`)
 
     const theme = useTheme();
     const navigate = useNavigate()
@@ -18,6 +17,19 @@ const LoginView = () => {
     const [name, setName] = useState<string>()
     const [number, setNumber] = useState<string>()
     const [loading, setLoading] = useState<boolean>(false)
+    const [width, setWidth] = useState<number>()
+    const [height, setHeight] = useState<number>()
+
+    useEffect(() => {
+        window.addEventListener('resize', () => {
+            let viewportWidth = window.innerWidth;
+            let viewportHeight = window.innerHeight;
+            console.log(`viewportWidth ${viewportWidth} viewportHeight ${viewportHeight}`)
+            setWidth(viewportWidth)
+            setHeight(viewportHeight)
+        })
+    }, [])
+
     const onLoginClick = async () => {
         if (name && number) {
             setLoading(true)
@@ -36,19 +48,16 @@ const LoginView = () => {
         return <Loading />
     }
     return <>
-        <Box
-            display="flex"
-            flexDirection="column"
-            justifyContent="center"
-            alignItems="center"
-            minHeight="90vh"
-        >
-            <Typography variant="h4" fontStyle="bold" color={theme.palette.primary.main} sx={{ mb: 5 }}>나에게 쓰는 편지</Typography>
-            <TextField onChange={(e) => setName(e.target.value)} sx={{ width: STYLE.WIDTH, m: 1 }} label={LABEL.NAME} />
-            <TextField onChange={(e) => setNumber(e.target.value)} sx={{ width: STYLE.WIDTH, m: 1 }} label={LABEL.NUMBER} />
-            <Button variant="contained" sx={{ width: STYLE.WIDTH, height: 70, m: 1 }} onClick={onLoginClick}>로그인</Button>
-        </Box >
+        <Box sx={{ display: "flex", flexDirection: "column", alignItems: "center", justifyContent: "center", m: 0, p: 0 }}>
+            <Box
+                sx={{ background: `url(${bgLogin})`, backgroundSize: "cover", width: 1920, minWidth: 1920, height: "98vh", display: "flex", flexDirection: "column", alignItems: "center", justifyContent: "center", m: 0, p: 0 }}
 
+            >
+                <TextField onChange={(e) => setName(e.target.value)} sx={{ width: STYLE.WIDTH, m: 1, fontFamily: "Bold" }} label={LABEL.NAME} />
+                <TextField onChange={(e) => setNumber(e.target.value)} sx={{ width: STYLE.WIDTH, m: 1 }} label={LABEL.NUMBER} />
+                <Button variant="contained" sx={{ width: STYLE.WIDTH, height: 70, m: 1 }} onClick={onLoginClick}>로그인</Button>
+            </Box >
+        </Box>
     </>
 }
 
