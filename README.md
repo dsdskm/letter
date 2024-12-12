44f6862a811236d1f19312ebb7d8def8c235d136
김기훈/선임연구원/로봇서비스개발Project(kkh.kim@lge.com) <tothetg@naver.com>
2024-12-12 16:56:56 +0900

    patch

--- README.md
+++ README.md
@@ -1,5 +1,3 @@
 ## build
 npm run build
-firebase target:clear hosting thankstape
-firebase target:apply hosting thankstape thankstape
-firebase deploy --only hosting:thankstape
+firebase deploy --only hosting
--- src/common/firebaseConfig.ts
+++ src/common/firebaseConfig.ts
@@ -1,16 +1,21 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
-import { getStorage } from "firebase/storage"
 import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyBeTUJj0spa3mnNcNTqEs_vVh1KH-Bc7HE",
-  authDomain: "mydiary-dev-5daf0.firebaseapp.com",
-  projectId: "mydiary-dev-5daf0",
-  storageBucket: "mydiary-dev-5daf0.appspot.com",
-  messagingSenderId: "342289437608",
-  appId: "1:342289437608:web:4947bcb393eefc4facc39d",
-  measurementId: "G-6T43PWNLRS"
+  apiKey: "AIzaSyBsUSFPyCnfszSiPNjtGhRRJ1M4morPxLY",
+  authDomain: "thankstape-3c117.firebaseapp.com",
+  projectId: "thankstape-3c117",
+  storageBucket: "thankstape-3c117.firebasestorage.app",
+  messagingSenderId: "703633262883",
+  appId: "1:703633262883:web:7eb4ed9bdf18de4c10a496",
+  measurementId: "G-3DN387BC0F"
 };
 
 // Initialize Firebase
